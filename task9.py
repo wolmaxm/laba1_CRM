10e4ec26952d6dc5516fbc522e4cede7d505adf0
Maksym <maksim144slay@gmail.com>
2024-09-23 09:29:16 +0300

    Updated task9.py

--- task9.py
+++ task9.py
@@ -4,10 +4,7 @@ N=int(input("Enter the N number (N>0): "))
 if N <= 0:
     print("The N number must be more than 0")
 else:
-    rev_N = 0
+    N=str(N)
+    n_rev=N[::-1]
+    print(n_rev)
     
-while N > 0:
-    i= N % 10
-    rev_N = rev_N * 10 + i
-    N = N // 10 
-    print(rev_N)
