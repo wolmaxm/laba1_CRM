9ed06e36422d36fb463cfcc43bfd68b7a803fbd1
Maksym <maksim144slay@gmail.com>
2024-09-23 09:30:33 +0300

    Updated task7.py

--- task7.py
+++ task7.py
@@ -7,9 +7,11 @@ x2=int(input("Введіть третю координату x2: "))
 y2=int(input("Введіть четверту координату y2: "))
 
 # queen is walking ONLY diagonally (y1=y2), vertically (x1=x2) or horizontally (x2-x1==y2-y1)
-
-if y1==y2 or x1==x2 or (abs(x2-x1)==abs(y2-y1)):
-    print("Queen can walk like that")
+if x1==x2 and y1==y2:
+    print("Queen is standing on its position")
 else:
-    print("Queen can not walk like that")
+     if y1==y2 or x1==x2 or (abs(x2-x1)==abs(y2-y1)):
+         print("Queen can walk like that")
+     else:
+         print("Queen can not walk like that")
     
